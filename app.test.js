554c4aa9ee87cd73b80065c891255c8d0f55be48
Aramide Ramadan <92337603+mister-abdurahman@users.noreply.github.com>
2022-09-06 00:54:05 +0100

    Add files via upload

---
+++ app.test.js
@@ -0,0 +1,18 @@
+const supertest = require('supertest');
+const server = require('../../app');
+
+
+describe("Calculate", () => {
+    it('POST /calculate: action: sum', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'sum',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+        
+    })
+})
